3cc4b93fba563172c3b55d273692a7b7eb771932
Ernesto Cullen <ernesto.cullen@environicsanalytics.com>
2024-01-01 16:16:01 -0500

    multiple threads

--- MultipleThreads/Program.cs
+++ MultipleThreads/Program.cs
@@ -3,6 +3,7 @@
 const long MaxCount = 100_000_000;
 const int NumProcesses = 4;
 
+
 static long Count(long maxCount)
 {
     var counter = 0;
@@ -14,14 +15,15 @@ static long Count(long maxCount)
     return counter;
 }
 
+Console.WriteLine("Starting threads, no pool");
 var stopwatch = new Stopwatch();
 stopwatch.Start();
 
+// create new threads without a pool
 var threads = new List<Thread>();
 for (int i = 0; i < NumProcesses; i++)
 {
-    
-    var proc = new Thread(()=>Count(MaxCount));
+    var proc = new Thread(() => Count(MaxCount));
     threads.Add(proc);
 }
 
@@ -37,3 +39,4 @@ foreach (var proc in threads)
 
 stopwatch.Stop();
 Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
+
--- readme.md
+++ readme.md
@@ -2,5 +2,5 @@
 
 Examples ported from [Comprehensive Guide to Concurrency and Parallelism in Python](https://towardsdatascience.com/comprehensive-guide-to-concurrency-and-parallelism-in-python-415ee5fbec1a)
 
-Trying to get the same functionality as the original examples in the article, but in C#.
+First example: multiprocessing. Spawning multiple processes to run the same code in parallel.
 
