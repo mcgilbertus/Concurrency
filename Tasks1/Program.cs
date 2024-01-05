9caf8894a2445c5c490589d3bac53d3a7fb903f2
Ernesto Cullen <ernesto.cullen@environicsanalytics.com>
2024-01-05 14:45:07 -0500

    added project demo with timeout cancellation token

---
+++ Tasks1/Program.cs
@@ -0,0 +1,26 @@
+﻿async Task ReadWebPage(string url)
+{
+    using var tokenSource = new CancellationTokenSource();
+    tokenSource.CancelAfter(10);
+
+    try
+    {
+        using var client = new HttpClient();
+        var html = await client.GetStringAsync(url, tokenSource.Token);
+        Console.WriteLine(html);
+    }
+    catch (OperationCanceledException)
+    {
+        Console.WriteLine("The request was canceled");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception {ex.GetType()} occurred. Message: {ex.Message}");
+    }
+}
+
+Console.WriteLine("Reading web page...");
+ReadWebPage("https://www.linkedin.com/");
+Console.WriteLine("Ready for user input");
+Console.ReadLine();
+
